8d18a926d756888674a7bcb3904bc3f66cb43e4a
Maria Luisa <maluisa.rc@gmail.com>
2017-10-22 18:22:55 -0700

    Add styling

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-
+  showMenu();
 });
+
+var showMenu = function(){
+  $('.menu-container').on('click', function() {
+    $('#index-menu').toggleClass('index-menu-on');
+  });
+};
+
+
+
+//   $('.menu-container').toggle(
+//     function() {
+//       $('.location-form').css("display", "inline-block");
+//       $('.index-menu').css("display", "inline-block");
+//     },
+//     function() {
+//       $('.location-form').css("display", "block");
+//       $('.index-menu').css("display", "none");
+//     });
+// };
+
+
+
+
 
 
